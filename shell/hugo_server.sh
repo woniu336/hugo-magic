0bb9b035870b29caa15a949df73c1ac187ba119e
woniu336 <310478073@qq.com>
2024-08-18 18:50:07 +0800

    update

--- shell/hugo_server.sh
+++ shell/hugo_server.sh
@@ -1,2 +1,2 @@
 cd ..
-hugo server -w -D -p 1313 --bind 0.0.0.0 --contentDir content --minify --forceSyncStatic --ignoreCache --noHTTPCache --gc -e production
+hugo server -w -D -p 1314 --bind 0.0.0.0 --contentDir content --minify --forceSyncStatic --ignoreCache --noHTTPCache --gc -e production
